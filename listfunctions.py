92b75e5a386723f55724bbc0bf32246f24f4c381
omae26 <omaekemunto26@gmail.com>
2025-04-21 21:49:44 +0300

    count,index, pop,clear functiond

--- listfunctions.py
+++ listfunctions.py
@@ -2,7 +2,7 @@
 # Using functions with lists in python
 
 lucky_numbers = [4, 8, 15, 16, 23, 42]
-friends = ["Ross", "Rachel", "Monica", "Chandler", "Joey", "Phoebe"]
+friends = ["Ross", "Rachel", "Monica", "Chandler", "Joey", "Joey", "Phoebe"]
 
 # List functions
 # extend() - adds elements of a list to another list i.e allows you to take a listand append another list onto the end of it
@@ -18,4 +18,30 @@ friends.insert(2, "Janice")
 #remove() - removes a specified element from a list
 #It holds 1 parameter, the element you want to remove from the list
 friends.remove("Janice")
+
+#index() - returns the index of  a specified element in a list
+#It holds 1 parameter, the element you want to find the index of in the list
+#It returns the index of the first occurrence of the element in the list
+print(friends.index("Rachel"))
+
+#count() - returns the number of occurrences of a specified element in a list
+#It holds 1 parameter, the element you want to count in the list
+print(friends.count("Joey"))
+
+#pop() - removes the last element from a list and returns it
+#It holds 1 parameter, the index of the element you want to remove from the list
+print(friends.pop()) #removes the last element from the list and returns it
+
+
+#clear() - removes all elements from a list
+#It holds no parameters
+friends.clear()
+lucky_numbers.clear()
+print(lucky_numbers)
+
+
+
+
+
 print(friends)
+
