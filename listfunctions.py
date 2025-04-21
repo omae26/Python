55cac61610b6cb4f56ec0873fa3c160f6a5b8146
omae26 <omaekemunto26@gmail.com>
2025-04-21 21:58:05 +0300

    sort, copy,and reverse functions

--- listfunctions.py
+++ listfunctions.py
@@ -1,10 +1,26 @@
 # 21/04.2025
 # Using functions with lists in python
 
-lucky_numbers = [4, 8, 15, 16, 23, 42]
+lucky_numbers = [4, 8, 15, 16, 23, 2]
 friends = ["Ross", "Rachel", "Monica", "Chandler", "Joey", "Joey", "Phoebe"]
 
 # List functions
+#sort() - sorts the elements of a list in ascending order
+friends.sort() # alphabetically
+lucky_numbers.sort()
+print(friends)
+print(lucky_numbers)
+
+# copy() - creates a copy of a list
+friends2 = friends.copy()
+print(friends2)
+
+# reverse() - reverses the order of the elements in a list
+friends.reverse() # reverses the order of the elements in the list
+lucky_numbers.reverse() # reverses the order of the elements in the list
+print(friends)
+print(lucky_numbers)
+
 # extend() - adds elements of a list to another list i.e allows you to take a listand append another list onto the end of it
 friends.extend(lucky_numbers)
 
@@ -27,6 +43,8 @@ print(friends.index("Rachel"))
 #count() - returns the number of occurrences of a specified element in a list
 #It holds 1 parameter, the element you want to count in the list
 print(friends.count("Joey"))
+
+
 
 #pop() - removes the last element from a list and returns it
 #It holds 1 parameter, the index of the element you want to remove from the list
