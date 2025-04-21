556201474acf6afc6f9bc02891782fac134382d7
omae26 <omaekemunto26@gmail.com>
2025-04-21 21:31:50 +0300

    How to use functions while working with lists

---
+++ listfunctions.py
@@ -0,0 +1,7 @@
+# 21/04.2025
+# Using functions with lists in python
+
+lucky_numbers = [4, 8, 15, 16, 23, 42]
+friends = ["Ross", "Rachel", "Monica", "Chandler", "Joey", "Phoebe"]
+
+print(friends)
