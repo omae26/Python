890fc12598ad1d2099ea530cbd82844cc4c400ee
omae26 <omaekemunto26@gmail.com>
2025-04-22 23:11:43 +0300

    Functions have to be called for them to be executed

---
+++ functions.py
@@ -0,0 +1,15 @@
+# 22/04/2025
+# A function is a collection of ststements whic perform a specific task.
+# Functions are used to break down a program into smaller, more manageable pieces.
+# Functions can take inputs (arguments) and return outputs (results).
+
+# Functions can be defined using the def keyword, followed by the function name and parentheses.
+
+# A function that says hello to the user
+def say_hi(): # After the colons, all the code that comes after is going to be part of the function. We therefore have to indent it.
+    print("Hello user!")
+
+# For us to execture the function, we have to call it. 
+# This is done by writing the function name followed by parentheses.
+
+say_hi() # Output: Hello user!
