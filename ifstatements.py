517ca9c34f717d99a6d3dbf44fecf78da77ba8f7
omae26 <162116251+omae26@users.noreply.github.com>
2025-04-24 23:11:49 +0300

    Update ifstatements.py

--- ifstatements.py
+++ ifstatements.py
@@ -1,2 +1,6 @@
 # 24/04/2025
-# If statements help programs to make decisions based on conditions
+#is_male = True
+if is_male: 
+    print("You are a male")
+else:
+    print("You are not a male") If statements help programs to make decisions based on conditions
