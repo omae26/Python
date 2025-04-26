e9dc899cf14266ca81582dcd18bdf5ca36080d1f
omae26 <162116251+omae26@users.noreply.github.com>
2025-04-26 21:59:54 +0300

    Update tuples.py

--- tuples.py
+++ tuples.py
@@ -1,14 +1,14 @@
 #22/04/2025
 
 # A tuple is a type of data structure. This means that it is a container that can store different values.
-# Tuples are similar to list, but they have some key differences.
+# Tuples are similar to list, but they have some key differences. They main one being, once data is added to a tuple it can't be changed, but for lists you can change values.
 
-# How create a tuple
+# How to create a tuple
 # A tuple is created by placing all the items (elements) inside parentheses (), separated by commas.
 # Tuples are immutable, meaning that you cannot change, add, or remove items once the tuple is created.
 # Tuples allow duplicate values.
 # Tuples can contain different data types, such as strings, integers, and booleans.
-# Tuples can be empty, meaning they contain no items.
+# Tuples can be empty, meaning they can contain no items.
 
 coordinates = (4, 5)
 #coordinates[1] = 10  # This will raise a TypeError because tuples are immutable
