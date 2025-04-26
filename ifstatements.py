019b6349f755efabada66fb9b9b73c4f958653c9
omae26 <162116251+omae26@users.noreply.github.com>
2025-04-26 21:46:35 +0300

    Update ifstatements.py
    An example done

--- ifstatements.py
+++ ifstatements.py
@@ -1,6 +1,14 @@
 # 24/04/2025
-#is_male = True
+
+is_male = True
 if is_male: 
     print("You are a male")
 else:
-    print("You are not a male") If statements help programs to make decisions based on conditions
+    print("You are not a male") 
+#if statements help programs to make decisions based on conditions
+
+sunday = True
+if sunday:
+    print("I'll go to church")
+else:
+    print("I'll go to school")
