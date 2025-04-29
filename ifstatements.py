e8611433f2f54c185d609060259281faa867f6ba
omae26 <omaekemunto26@gmail.com>
2025-04-29 22:33:22 +0300

    opeartors whileusing if statements

--- ifstatements.py
+++ ifstatements.py
@@ -1,17 +1,12 @@
 # 24/04/2025
-<<<<<<< HEAD
 # If statements help programs to make decisions based on conditions
 # Example: Boolean
-=======
 
->>>>>>> 4676ebc17bff40c8e084cf1dcd4bbfe918525a80
 is_male = True
 if is_male: 
     print("You are a male")
 else:
-<<<<<<< HEAD
     print("You are not a male")
-=======
     print("You are not a male") 
 #if statements help programs to make decisions based on conditions
 
@@ -20,4 +15,32 @@ if sunday:
     print("I'll go to church")
 else:
     print("I'll go to school")
->>>>>>> 4676ebc17bff40c8e084cf1dcd4bbfe918525a80
+
+is_male = True
+is_tall = False
+# If both are true, it will print 'You are a tall male'
+# If is_male is false, it will print 'You are not a male but tall'
+# If is_male is true, and is_tall false ,it will print 'You are a short male'
+# If both are false, it will print 'You are not male and not tall'
+# OR operator 
+if is_male or is_tall:
+    print("You are male or tall or both ")
+else:
+    print("You are neither male or tall")
+
+# AND operator. It works when both conditions are true    
+is_male = True
+is_tall = True
+# OR operator 
+if is_male or is_tall:
+    print("You are a tall male ")
+
+# Adding another condition using else if (elif)
+# To check if they are male but not tall
+elif is_male and not(is_tall): # not function negates what comes after it
+    print("You are a short male")
+# Another condition to check if they are not male but tall
+elif not(is_male) and (is_tall): # not function negates what comes after it
+    print("You are not a male but tall")
+else:
+    print("You are not male and not tall ")
