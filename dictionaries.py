0c17ad0902ea47989c284f01a9c70b2fee9d77a0
omae26 <omaekemunto26@gmail.com>
2025-05-02 23:54:26 +0300

    dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,21 @@
+# 2/05/2025
+# Dictionaries allow us to store infromation in key-value pairs.
+# They are mutable, unordered, and indexed by keys.
+
+# Example: Converting a 3-digit month name into a full-month name.
+# The month name (data) is the key and the full- month name is the value.
+
+monthConversions = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December"
+}
