edf6cbf4c2357db79d5cc5694327733df4469bfe
omae26 <162116251+omae26@users.noreply.github.com>
2025-05-08 22:59:05 +0300

    Update gettinginputfromusers.py

--- gettinginputfromusers.py
+++ gettinginputfromusers.py
@@ -6,7 +6,7 @@
 # The input function is a blocking function, meaning it will wait for the user to enter a value before continuing with the program
 # The input function is a built-in function in Python, so we don't need to import it
 
-#The examples below shows how the input obtained from the user is stored in a variable,then printed out
+#The examples below show how the input obtained from the user is stored in a variable,then printed out
 name = input("Enter your name: ")
 age = input("Enter your age: ")
 print("Hello " + name + "! You are " + age + " years old.")
