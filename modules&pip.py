2d10f0a6d7dbf63658d3036b054c9781fb901b8a
omae26 <omaekemunto26@gmail.com>
2025-05-26 15:27:37 +0300

    modules

---
+++ modules&pip.py
@@ -0,0 +1,8 @@
+# 26/05/2025
+# A module is essentially a python file that we can import into our current pyhton file.
+# We have built in and external modules, but we can also create our own.
+# The other file we're using is useful.py
+
+import useful
+
+print(useful.roll_dice(10))
